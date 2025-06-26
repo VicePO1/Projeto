c5345b0fbb940ef946d57a165b238e4861e6e355
VicePO1 <viclindo11@gmail.com>
2025-06-26 18:14:36 +0100

    Add files via upload

---
+++ samples.py
@@ -0,0 +1,16 @@
+import random
+numbersR=random.sample(range(1, 51), 15)
+number=0
+order=0
+
+number=numbersR[order]
+order=order+1
+print(number)
+
+number=numbersR[order]
+order=order+1
+print(number)
+
+number=numbersR[order]
+order=order+1
+print(number)
